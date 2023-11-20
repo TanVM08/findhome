0327e3206750966c829315fbbe0159733d5f290d
TanVM <vuminhtan0103@gmail.com>
2023-11-20 11:10:02 +0700

    footer and go to top

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
-
+import { Component, HostListener } from '@angular/core';
+import * as $ from 'jquery';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'tim-tro';
+
+  gotoTop() {
+    window.scroll({
+      top: 0,
+      left: 0,
+      behavior: 'smooth',
+    });
+  }
+
+  @HostListener('window:scroll', []) onWindowScroll() {
+    // do some stuff here when the window is scrolled
+    let pos = window.pageYOffset;
+    if (pos > 50) {
+      $('#goTop').css('display', 'block');
+    } else {
+      $('#goTop').css('display', 'none');
+    }
+  }
 }
